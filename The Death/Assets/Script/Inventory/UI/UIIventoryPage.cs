10613ff3439fad0862c1b7393d7240fce24958b4
ThanhSon-3112 <son54029@gmail.com>
2024-03-19 22:04:46 +0700

    inventory

--- The Death/Assets/Script/Inventory/UI/UIIventoryPage.cs
+++ The Death/Assets/Script/Inventory/UI/UIIventoryPage.cs
@@ -1,38 +1,168 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class UIIventoryPage : MonoBehaviour
+namespace Inventory.UI
 {
-    [SerializeField] private UIInventoryItem itemPrefab;
-    [SerializeField] private RectTransform contentPanel;
-
-    List<UIInventoryItem> listOfUIItems = new List<UIInventoryItem>();
-
-    private Vector3 initialScale;
-
-    public void InitializeInventoryUI(int inventorysize)
+    public class UIIventoryPage : MonoBehaviour
     {
-        initialScale = itemPrefab.transform.localScale;
+        [SerializeField] private UIInventoryItem itemPrefab;
+        [SerializeField] private RectTransform contentPanel;
+        [SerializeField] private UIInventoryDescription itemDescription;
+        [SerializeField] private MouseFollower mouseFollower;
 
-        for (int i = 0; i < inventorysize; i++)
+        List<UIInventoryItem> listOfUIItems = new List<UIInventoryItem>();
+
+        private int currentlyDraggedItemIndex = -1;
+
+        public event Action<int> OnDescriptionRequested,
+                OnItemActionRequested,
+                OnStartDragging;
+
+        public event Action<int, int> OnSwapItems;
+
+        private Vector3 initialScale;
+        public void InitializeInventoryUI(int inventorysize)
         {
-            UIInventoryItem uiItem = Instantiate(itemPrefab, Vector3.zero, Quaternion.identity);
-            uiItem.transform.SetParent(contentPanel);
-            // luu ti le cua item
-            uiItem.transform.localScale = initialScale;
-            listOfUIItems.Add(uiItem);
-        }
-    }
+            initialScale = itemPrefab.transform.localScale;
 
-    public void Show()
-    {
-        gameObject.SetActive(true);
-        Time.timeScale = 0f;
-    }
-    public void Hide()
-    {
-        gameObject.SetActive(false);
-        Time.timeScale = 1f;
+            for (int i = 0; i < inventorysize; i++)
+            {
+                UIInventoryItem uiItem = Instantiate(itemPrefab, Vector3.zero, Quaternion.identity);
+                uiItem.transform.SetParent(contentPanel);
+                // luu ti le cua item
+                uiItem.transform.localScale = initialScale;
+                listOfUIItems.Add(uiItem);
+
+                uiItem.OnItemClicked += HandleItemSelection;
+                uiItem.OnItemBeginDrag += HandleBeginDrag;
+                uiItem.OnItemDroppedOn += HandleSwap;
+                uiItem.OnItemEndDrag += HandleEndDrag;
+                uiItem.OnRightMouseBtnClick += HandleShowItemActions;
+            }
+        }
+
+        internal void ResetAllItems()
+        {
+            foreach (var item in listOfUIItems)
+            {
+                item.ResetData();
+                item.Deselect();
+            }
+        }
+
+        internal void UpdateDescription(int itemIndex, Sprite itemImage, string name, string description)
+        {
+            itemDescription.SetDescription(itemImage, name, description);
+            DeselectAllItems();
+            listOfUIItems[itemIndex].Select();
+        }
+
+        public void UpdateData(int itemIndex,
+               Sprite itemImage, int itemQuantity)
+        {
+            if (listOfUIItems.Count > itemIndex)
+            {
+                listOfUIItems[itemIndex].SetData(itemImage, itemQuantity);
+            }
+        }
+
+        private void Awake()
+        {
+            Hide();
+            mouseFollower.Toggle(false);
+            itemDescription.ResetDescription();
+        }
+
+        private void HandleShowItemActions(UIInventoryItem inventoryItemUI)
+        {
+            int index = listOfUIItems.IndexOf(inventoryItemUI);
+            if (index == -1)
+            {
+                return;
+            }
+            OnItemActionRequested?.Invoke(index);
+        }
+
+        private void HandleEndDrag(UIInventoryItem inventoryItemUI)
+        {
+            mouseFollower.Toggle(false);
+
+        }
+
+        private void HandleSwap(UIInventoryItem inventoryItemUI)
+        {
+            int index = listOfUIItems.IndexOf(inventoryItemUI);
+            if (index == -1)
+            {
+                return;
+            }
+            OnSwapItems?.Invoke(currentlyDraggedItemIndex, index);
+            HandleItemSelection(inventoryItemUI);
+        }
+
+        private void ResetDraggedItem()
+        {
+            mouseFollower.Toggle(false);
+            currentlyDraggedItemIndex = -1;
+        }
+
+        private void HandleBeginDrag(UIInventoryItem inventoryItemUI)
+        {
+            int index = listOfUIItems.IndexOf(inventoryItemUI);
+            if (index == -1)
+                return;
+            currentlyDraggedItemIndex = index;
+            HandleItemSelection(inventoryItemUI);
+            OnStartDragging?.Invoke(index);
+        }
+
+        public void CreateDraggedItem(Sprite sprite, int quantity)
+        {
+            mouseFollower.Toggle(true);
+            mouseFollower.SetData(sprite, quantity);
+        }
+
+        private void HandleItemSelection(UIInventoryItem inventoryItemUI)
+        {
+            int index = listOfUIItems.IndexOf(inventoryItemUI);
+            if (index == -1)
+                return;
+            OnDescriptionRequested?.Invoke(index);
+        }
+
+        public void Show()
+        {
+            gameObject.SetActive(true);
+            Time.timeScale = 0f;
+            ResetSelection();
+
+        }
+
+        public void ResetSelection()
+        {
+            itemDescription.ResetDescription();
+            DeselectAllItems();
+        }
+
+        private void DeselectAllItems()
+        {
+            foreach (UIInventoryItem item in listOfUIItems)
+            {
+                item.Deselect();
+            }
+        }
+
+        public void Hide()
+        {
+            gameObject.SetActive(false);
+            Time.timeScale = 1f;
+            ResetDraggedItem();
+        }
+
+
+
     }
 }
