4beb4e7552088f5725e21052ac6dbd0491ffac4a
ThanhSon-3112 <son54029@gmail.com>
2024-09-28 21:15:26 +0700

    fix meteo

--- The Death/Assets/_Script/PlayerSkill/Meteo.cs
+++ The Death/Assets/_Script/PlayerSkill/Meteo.cs
@@ -27,6 +27,12 @@ public class Meteo : MonoBehaviour
         player = GameObject.FindGameObjectWithTag("Player");
         Invoke("ReturnToMeteo", lifeTime);
         FindTargetEnemy();
+
+        // Ki?m tra n?u không có m?c tiêu, phá h?y thiên th?ch sau m?t th?i gian
+        if (targetEnemy == null)
+        {
+            DestroyMeteo();
+        }
     }
 
     private void FindTargetEnemy()
@@ -63,11 +69,11 @@ public class Meteo : MonoBehaviour
             // Flip theo h??ng enemy
             if (moveDirection.x < 0f)
             {
-                transform.localScale = new Vector3(-Mathf.Abs(transform.localScale.x), transform.localScale.y, transform.localScale.z);
+                transform.localScale = new Vector3(Mathf.Abs(transform.localScale.x), transform.localScale.y, transform.localScale.z);
             }
             else
             {
-                transform.localScale = new Vector3(Mathf.Abs(transform.localScale.x), transform.localScale.y, transform.localScale.z);
+                transform.localScale = new Vector3(-Mathf.Abs(transform.localScale.x), transform.localScale.y, transform.localScale.z);
             }
 
             rb.velocity = moveDirection * speed;
@@ -92,5 +98,14 @@ public class Meteo : MonoBehaviour
     public void ReturnToMeteo()
     {
         MeteoPool.Instance.ReturnMeteo(gameObject);
+    }
+
+    private void DestroyMeteo()
+    {
+        // T?o hi?u ?ng n? tr??c khi phá h?y
+        Instantiate(explosionPrefab, transform.position, Quaternion.identity);
+
+        // Tr? v? pool ho?c phá h?y n?u không có pool
+        MeteoPool.Instance.ReturnMeteo(gameObject); // Ho?c s? d?ng Destroy(gameObject) n?u không có pool
     }
 }
